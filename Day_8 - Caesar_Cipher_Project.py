bfbceb75b993a11250f02a3872eb604295e05185
tallevy95 <77324590+tallevy95@users.noreply.github.com>
2023-03-19 18:10:31 +0200

    Update Day_8 - Caesar_Cipher_Project.py

--- Day_8 - Caesar_Cipher_Project.py
+++ Day_8 - Caesar_Cipher_Project.py
@@ -1,3 +1,7 @@
+"""
+In this assignment I wrote a code that implemented the Caesar Cipher encryption technique.
+"""
+
 from art import logo_Caesar_Cipher
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
